973f50a0cb88eab70a290e7f825d5a307599c90a
Greg Ross <greg@toolstack.com>
2024-03-25 11:43:12 -0400

    Fix strlen in generating the tables
    And cleanup the MD table generation

--- release/generate.supported.languages.php
+++ release/generate.supported.languages.php
@@ -4,6 +4,7 @@
 	$banners = array( 'DeepL' => 'DeepL', 'Google Translate' => 'Google Translate', 'Microsoft Translator' => 'Microsoft Translator', 'Yandex.Translate' => '<a href="http://translate.yandex.com/" target="_blank">Powered by Yandex.Translate</a>' );
 	$includes = array( 'DeepL' => 'deepl.locales.php', 'Yandex.Translate' => 'yandex.locales.php', 'Microsoft Translator' => 'microsoft.locales.php', 'Google Translate' => 'google.locales.php' );
 
+	// This should be the path to a GlotPress locales file.
 	include( '../../../glotpress/GlotPress/locales/locales.php' );
 
 	$gp_locales = new GP_Locales;
@@ -13,7 +14,7 @@
 	$width = 0;
 
 	foreach( $providers as $provider ) {
-		if( strlen( $provider ) > $width ) { $width = strlen( $provider ); }
+		if( mb_strlen( $provider ) > $width ) { $width = mb_strlen( $provider ); }
 
 		$gp_machine_translate_locales = array();
 
@@ -21,7 +22,7 @@
 
 		foreach( $gp_machine_translate_locales as $mt_locale => $p_locale ) {
 			if( array_key_exists( $mt_locale, $locales ) ) {
-				if( strlen( $locales[$mt_locale]->english_name ) > $width ) { $width = strlen( $locales[$mt_locale]->english_name ); }
+				if( mb_strlen( $locales[$mt_locale]->english_name ) > $width ) { $width = mb_strlen( $locales[$mt_locale]->english_name ); }
 
 				$storage[$provider][] = $locales[$mt_locale]->english_name;
 
@@ -30,14 +31,14 @@
 		}
 	}
 
-	$output = '';
-	$markdown = '';
+	$output = '| ';
+	$markdown = '| ';
 	$wp_md = "\t";
 
 	foreach( $providers as $provider ) {
 		sort( $storage[$provider] );
 
-		$padding = str_repeat( ' ', $width - strlen( $provider )  );
+		$padding = str_repeat( ' ', $width - mb_strlen( $provider )  );
 
 		$output .= $provider . $padding . '  ';
 		$markdown .= $provider . $padding . ' | ';
@@ -48,13 +49,15 @@
 	$markdown = rtrim( $markdown ) . PHP_EOL;
 	$wp_md = rtrim( $wp_md ) . PHP_EOL;
 
+	$output .= '|';
+	$markdown .= '|';
 	$wp_md .= "\t";
 
 	foreach( $providers as $provider ) {
 		$padding = str_repeat( '-', $width );
 
 		$output .= $padding . '  ';
-		$markdown .= $padding . '-|-';
+		$markdown .= $padding . '--|';
 		$wp_md .= $padding . '  ';
 	}
 
@@ -64,13 +67,13 @@
 
 	for( $i = 0; $i < $length; $i++ ) {
 		$line = '';
-		$mline = '';
+		$mline = '| ';
 
 		foreach( $providers as $provider ) {
 			if( array_key_exists( $i, $storage[$provider] ) ) {
 				$locale = $storage[$provider][$i];
-				$line .= $locale . str_repeat( ' ', $width - strlen( $locale )  ) . '  ';
-				$mline .= $locale . str_repeat( ' ', $width - strlen( $locale )  ) . ' | ';
+				$line .= $locale . str_repeat( ' ', $width - mb_strlen( $locale )  ) . '  ';
+				$mline .= $locale . str_repeat( ' ', $width - mb_strlen( $locale )  ) . ' | ';
 			} else {
 				$line .= str_repeat( ' ', $width ) . '  ';
 				$mline .= str_repeat( ' ', $width ) . ' | ';
@@ -81,6 +84,22 @@
 		$markdown .= rtrim( $mline ) . PHP_EOL;
 		$wp_md .= "\t" . rtrim( $line ) . PHP_EOL;
 	}
+
+	$output .= '|';
+	$markdown .= '|';
+	$wp_md .= "\t";
+
+	foreach( $providers as $provider ) {
+		$padding = str_repeat( '-', $width );
+
+		$output .= $padding . '  ';
+		$markdown .= $padding . '--|';
+		$wp_md .= $padding . '  ';
+	}
+
+	$output = rtrim( $output ) . PHP_EOL;
+	$markdown = rtrim( $markdown ) . PHP_EOL;
+	$wp_md = rtrim( $wp_md ) . PHP_EOL;
 
 	file_put_contents( 'provider-chart.ascii.txt', $output );
 	file_put_contents( 'provider-chart.markdown.txt', $markdown );
